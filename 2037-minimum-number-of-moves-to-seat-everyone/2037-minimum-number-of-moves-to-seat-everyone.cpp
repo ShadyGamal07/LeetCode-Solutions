d0cbd7398f03ffd6a27fabd88fbb3ba194fb8add
Shady Gamal <48739505+ShadyGamal07@users.noreply.github.com>
2022-07-09 15:39:57 +0200

    Time: 8 ms (89.56%), Space: 18.2 MB (50.79%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& arr1, vector<int>& arr2) {
+        
+        sort(arr1.begin() , arr1.end());
+        sort(arr2.begin() , arr2.end());
+        
+        int ans=0;
+        
+        for(int i=0 ; i<arr1.size() ; i++){
+            ans+=(abs(arr1[i] - arr2[i]));
+        }
+        
+        return ans;
+        
+        
+    }
+};
